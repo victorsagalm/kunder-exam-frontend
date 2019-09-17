bd713d27903f8894549d25799814db87084a3d7a
victorsagalm <victor.sagal31@gmail.com>
2019-09-17 13:09:56 -0300

    Add navbar and separate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from '../components/home/home.component';
 import { BannerComponent } from '../components/banner/banner.component';
+import { NavbarComponent } from '../components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    BannerComponent
+    BannerComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { BannerComponent } from './banner/banner.component';
+import { NavbarComponent } from './navbar/navbar.component';
 @NgModule(
     { 
         declarations: [
             HomeComponent, 
-            BannerComponent], 
+            BannerComponent, NavbarComponent], 
         imports: [], 
         exports: [
             HomeComponent]
